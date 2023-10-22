b29eb53b184aa3dcfe25b6eed60aa670a4318760
persik <108504553+Alyona1619@users.noreply.github.com>
2023-10-22 18:34:41 +0800

    Create Config.h

---
+++ ESP_Lamp/Config.h
@@ -0,0 +1,10 @@
+String AP_NAME = "ESP8266";
+String AP_PASSWORD = "ESP8266123"; //0 пункт 6 задание
+
+char* CLI_SSID = " "; //5 -> 2.4Ghz
+char* CLI_PASS = " ";
+
+//int WEB_SERVER_PORT = 80;
+
+const int mqtt_port = 1883;
+char* mqtt_broker = "broker.emqx.io";
