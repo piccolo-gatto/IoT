2ed96f5b3d4415a4b51b4989d101b7ab22cb5711
persik <108504553+Alyona1619@users.noreply.github.com>
2023-10-22 22:14:56 +0800

    Update pub.py

--- ESP_Lamp/pub.py
+++ ESP_Lamp/pub.py
@@ -1,1 +1,33 @@
+import time
 
+counter_start = 0
+counter_end = 0
+
+while True:
+    current_second = int(time.time() % 60)
+
+    if current_second == 20 + counter_start:
+        print(current_second, "sec")
+        print("start", counter_start, "end", counter_end)
+        print("Лампочка включена")
+        time.sleep((40 - counter_end)-(20 + counter_start))
+
+    elif current_second == 40 - counter_end:
+        print(current_second, "sec")
+        print("start", counter_start, "end", counter_end)
+        print("Лампочка выключена")
+
+        if counter_end % 2 == 1:
+            counter_start += 1
+
+        counter_end += 1
+        if counter_end == 6:
+            counter_end = 0
+
+        if counter_start == 6:
+            counter_start = 0
+
+        time.sleep(59 - ((40 - counter_end) - (20 + counter_start)))
+
+    else:
+        time.sleep(1) 
