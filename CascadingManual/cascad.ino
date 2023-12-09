30f726d8079af9ecfcc0caf8d8b02c77bf16103b
Ekaterina <108530800+piccolo-gatto@users.noreply.github.com>
2023-12-09 20:43:41 +0800

    Correct update

--- CascadingManual/cascad.ino
+++ CascadingManual/cascad.ino
@@ -4,62 +4,86 @@
 #define END_PIN 9
 
 SoftwareSerial next_node(10, 11);
-int num_node = 0;
 
+int num_node = 1;
+int buf = 0;
+int check_time = 0;
+ 
+// 2 3 4 5 6 7 8 9 
+// A B . C D E G F
 bool digits[10][8] = {
-  {1,1,0,1,1,1,0,1},  // 0
-  {0,1,0,1,0,0,0,0},  // 1
-  {1,1,0,0,1,1,1,0},  // 2
-  {1,1,0,1,1,0,1,0},  // 3
-  {0,1,0,1,0,0,1,1},  // 4
-  {1,0,0,1,1,0,1,1},  // 5
-  {1,0,1,1,1,1,1,1},  // 6
-  {1,1,0,1,0,0,0,0},  // 7
-  {1,1,0,1,1,1,1,1},  // 8
-  {1,1,1,1,1,0,1,1}   // 9
+  {1,1,0,1,1,1,0,1}, // 0
+  {0,1,0,1,0,0,0,0}, // 1
+  {1,1,0,0,1,1,1,0}, // 2
+  {1,1,0,1,1,0,1,0}, // 3
+  {0,1,0,1,0,0,1,1}, // 4
+  {1,0,0,1,1,0,1,1}, // 5
+  {1,0,1,1,1,1,1,1}, // 6
+  {1,1,0,1,0,0,0,0}, // 7
+  {1,1,0,1,1,1,1,1}, // 8
+  {1,1,1,1,1,0,1,1}  // 9
 };
 
 void setup() {
-  for(int i = START_PIN; i <= END_PIN; i++) {
-    pinMode(i, OUTPUT);
-    digitalWrite(i, HIGH);
+  for (int i = START_PIN; i <= END_PIN; i++) {
+  	pinMode(i, OUTPUT);
+   	digitalWrite(i, HIGH);
   }
-  Serial.begin(4800);
-  next_node.begin(4800);
-  num_node = 1; //write code to define it
+  Serial.begin(1200);
+  next_node.begin(1200);
+  
+  while (Serial.available() > 0) {
+    buf = Serial.read();
+  }
+  Serial.print("Nodes: ");
+  Serial.write('+');
+  while(next_node.available() == 0){
+  	delay(1);// отвалиться по тайммауту
+    if (millis() - check_time == 10){
+    	break;
+    }
+  }
+
+  while (next_node.available() > 0){ 
+    delay(20);
+
+    buf = next_node.read();
+    if(buf == int(char('+'))){  
+      num_node += 1;
+  	  Serial.write('+');
+    }
+  }
 }
 
 void loop() {
-  if(Serial.available() >= num_node) {
+
+  if (Serial.available() >= num_node) {
     char user_input = Serial.read();
     int digit = (int)(user_input - '0');
-    if (digit >= 0 && digit <= 9) {
-      show_digit(digit);
-      if (num_node == 1) {
-        delay(100);
-        while (Serial.available()) {
-          Serial.read();
-        }
-      }
-      while (Serial.available()) {
-        char received_char = Serial.read();
-        next_node.write(received_char);
-
-        if (received_char == 'n') {
-          int neighbors_count = num_node - 1;
-          char response_char = neighbors_count + '0';
-          next_node.write(response_char);
-        }
-      }
+    Serial.print(digit);
+    show_digit(digit);
+    while (Serial.available() > 0) {
+      next_node.write(Serial.read());
     }
+    delay(20);
+  }
+  delay(300);
+  if (Serial.available() > 0 && Serial.available() < num_node){
+    show_digit(0);
+    for (int i = 1; i < num_node-Serial.available(); i++) {
+      next_node.write('0');
+    }
+    while (Serial.available()) {
+      next_node.write(Serial.read());
+    } 
   }
 }
 
 void show_digit(int digit) {
-  if (digit < 0 || digit > 9) {
+  if (digit < 0 or digit > 9) {
     return;
   }
-  for(int i = START_PIN; i <= END_PIN; i++) {
-    digitalWrite(i, !digits[digit][i - START_PIN]);
-    }
+  for (int i = START_PIN; i <= END_PIN; i++) {
+    digitalWrite(i, !digits[digit][i-START_PIN]);
+  }
 }
